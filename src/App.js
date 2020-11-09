d7b1fa8497361895aa55bca2891fdff5ab74f78f
gorokhovPavel <pgorokhov@yandex.ru>
2020-11-10 01:35:23 +0300

    add lint, prettier

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TestComponent = () => <p>просто render</p>
+
+ const App = () => {
+    return (
+        <div className="App">
+            <header >
+               123
+            </header>
+        </div>
+    )
+}
+
+export default App
