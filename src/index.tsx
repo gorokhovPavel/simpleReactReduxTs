581925e4c88bea0425d735513d8cf3546420ed96
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2021-11-29 22:11:33 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -14,5 +14,5 @@ const store = createStore(reducer);
 ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+// unregister() to register() below. Note this comes with some pitfalls
 serviceWorker.unregister();
