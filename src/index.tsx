bcfdef43095315b2a37725a4ef196e48da98c91c
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2021-12-10 23:37:50 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,6 @@ import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducer);
 
-
 ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
