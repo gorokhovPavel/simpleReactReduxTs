3f531e80ce85666a775722abfd6c848674cae27a
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2021-12-10 23:37:56 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -15,4 +15,4 @@ ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls
-serviceWorker.unregister();
+serviceWorker.unregister()
