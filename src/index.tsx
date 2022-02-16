fc2d54ae822c339b4265f4a74d4f127c7381ef5e
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2022-02-16 18:50:20 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import './index.css';
 import App from './App';
 
 import * as serviceWorker from './serviceWorker';
-
 const store = createStore(reducer);
+
 ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
 serviceWorker.unregister()
