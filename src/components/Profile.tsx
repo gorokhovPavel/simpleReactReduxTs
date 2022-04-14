5cb0f60570804fb8432d020708a62d76fb6dd73e
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2022-04-14 08:19:44 +0300

    Update Profile.tsx

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,9 +3,7 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { IReducerState } from '../store/types'
 
-interface IProfileProps extends IStateToProps {
-
-}
+interface IProfileProps extends IStateToProps {}
 
 const profile:React.FC<IProfileProps> = (props) => {
 
