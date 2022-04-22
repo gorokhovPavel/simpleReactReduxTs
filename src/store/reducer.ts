1d466ee726787a2db36ade295e3df44e2c6cf14e
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2022-04-22 11:03:36 +0300

    Update reducer.ts

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -14,8 +14,7 @@ const initialState:IReducerState = {
 }
 
 const reducer = (state=initialState, action: IActionTypes): IReducerState => {
-    switch(action.type){
-            
+    switch(action.type) {
         case SUCCESSFUL_LOGIN:
             return {
                 ...state,
