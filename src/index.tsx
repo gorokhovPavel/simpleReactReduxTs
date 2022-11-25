7b7a3a6808c8f5b08a9a057dabb50ea73707a4ab
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2022-11-25 19:14:12 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { createStore } from 'redux';
 import reducer from './store/reducer';
 import { Provider } from 'react-redux';
