be7d1a10bc2885013c6e8f21c3848183eb4e1df6
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2022-11-25 19:14:01 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 
+
 import * as serviceWorker from './serviceWorker';
 const store = createStore(reducer);
 
